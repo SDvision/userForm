20be0f7987be65cc9dcf7a7b024bc5b8ad618731
SDvision <sanchit.dahatonde@gmail.com>
2021-06-08 19:17:08 +0530

    start of app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Employee_list from './components/Employee_list.jsx'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Employee_list />
     </div>
   );
 }
---
+++ src/components/Employee_list.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Fragment } from 'react'
+
+function Employee_list() {
+    return(
+        <Fragment>
+            Hi there
+        </Fragment>
+
+    )
+}
+export default Employee_list;
